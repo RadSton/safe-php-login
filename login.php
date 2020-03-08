447cdb5a382e304a77a2c079932c79213a9f7a4a
RadSton <55923402+RadSton@users.noreply.github.com>
2020-03-08 14:06:40 +0100

    Update login.php

--- login.php
+++ login.php
@@ -5,7 +5,7 @@ PHP MY ADMIN CODE TO CREATE A DB
 CREATE TABLE `users` (
   `id` INT NOT NULL AUTO_INCREMENT ,
   `email` VARCHAR(255) NOT NULL ,
-  `passwort` VARCHAR(255) NOT NULL ,
+  `password` VARCHAR(255) NOT NULL ,
   PRIMARY KEY (`id`), UNIQUE (`email`)
 ) ENGINE = InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 */
